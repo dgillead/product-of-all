fccbaeae50bb20b912fb2feb4525aa73b8b7f765
Darnell Gillead <dlgillead@gmail.com>
2017-06-27 08:19:21 -0500

    current solution, refactor to not use nested loops

--- products.js
+++ products.js
@@ -4,11 +4,18 @@
   // return all multiplied values
 
 function getAllProductsExceptIndex(arrayOfValues) {
+  let product = 1;
+  let productArray = [];
   for (let i = 0; i < arrayOfValues.length; i++) {
-    console.log(i);
-    console.log(arrayOfValues.splice(i, 1));
-    console.log(arrayOfValues);
+    for (let j = 0; j < arrayOfValues.length; j++) {
+      if (i != j) {
+        product *= arrayOfValues[j];
+      }
+    }
+    productArray.push(product);
+    product = 1;
   }
+  console.log(productArray);
 };
 
 getAllProductsExceptIndex([1, 7, 3, 4]);
